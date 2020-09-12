50e21c42588b81e9ef8a8a12124c98643bf72259
Kinnari Shah <kinnari1022@yahoo.com>
2020-09-12 17:21:08 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -47,11 +47,20 @@ class Student
     student.save
     student
   end
-  
-  def update
-  
-  
-  
+
+  def self.new_from_db(row)
+    #binding.pry
+Student.new(row[0], row[1], row[2])
   end
   
+  def self.find_by_name(name)
+    sql = "SELECT * FROM students WHERE name = ?"
+      result = DB[:conn].execute(sql, name)[0]
+      Student.new(result[0], result[1], result[2])
+  end
+  
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"  
+     DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end    
 end
